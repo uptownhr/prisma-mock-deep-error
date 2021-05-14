ae1ca1da8bda2bb2531f779b4799fb41589586d9
James Lee <uptownhr@gmail.com>
2021-05-14 12:05:41 -0700

    removed else

--- mock-deep.test.ts
+++ mock-deep.test.ts
@@ -1,11 +1,5 @@
-import {mockDeep, MockProxy} from "jest-mock-extended";
-import { PrismaClient } from '@prisma/client'
 import { prismaMock } from "./singleton";
 
 test('no tes error', () => {
-  const prisma = mockDeep<PrismaClient>();
-
-  const prismaMock2 = (prisma as unknown) as MockProxy<PrismaClient>
-
   jest.spyOn(prismaMock.user, 'findUnique');
 })
