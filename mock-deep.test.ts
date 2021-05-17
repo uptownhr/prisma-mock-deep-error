0fa4f0a932ec96954d24f0ae796403901702f769
Rich Haines <hello@richardhaines.dev>
2021-05-17 11:22:04 +0200

    Made some changes to the tsconfig and how the client is mocked

--- mock-deep.test.ts
+++
@@ -1,8 +0,0 @@
-import {mockDeep} from "jest-mock-extended";
-import { PrismaClient } from '@prisma/client'
-
-test('no tes error', () => {
-  const prismaMock = mockDeep<PrismaClient>();
-
-  const spyEmployeeExists = jest.spyOn(prismaMock.user, 'findUnique');
-})
